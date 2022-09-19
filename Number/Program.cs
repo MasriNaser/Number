b2d7aa64239270055a18556ee73d7217354fd839
MasriNaser <Naser.Masri@cronos.be>
2022-09-19 18:53:37 +0200

    init commit

---
+++ Number/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Number;
+
+
+internal class MyClass
+{
+    static void Main(string[] args)
+    {
+        string name = "Sanna";
+        Console.WriteLine(name);
+    }
+}
+
